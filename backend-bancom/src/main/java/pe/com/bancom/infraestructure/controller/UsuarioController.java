1988af9da792b02c647e999e735071f55eb1fa99
Ronald Angulo <ing.ronaldangulo@gmail.com>
2023-11-17 10:56:22 -0500

    Test listar usuarios en el controller pasa

---
+++ backend-bancom/src/main/java/pe/com/bancom/infraestructure/controller/UsuarioController.java
@@ -0,0 +1,27 @@
+package pe.com.bancom.infraestructure.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pe.com.bancom.domain.dto.UsuarioDto;
+import pe.com.bancom.domain.service.impl.UsuarioServiceImpl;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/usuarios")
+@AllArgsConstructor
+public class UsuarioController {
+
+    private final UsuarioServiceImpl usuarioService;
+
+    @GetMapping("/find/all")
+    public ResponseEntity<List<UsuarioDto>> findAll() {
+        log.info("find all");
+        return ResponseEntity.ok(usuarioService.findAll());
+    }
+}
