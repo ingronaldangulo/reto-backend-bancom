811785f14749659f9301e9396fa1541089cea3c5
Ronald Angulo <ing.ronaldangulo@gmail.com>
2023-11-17 07:36:09 -0500

    Linea base del proyecto

---
+++ backend-bancom/src/main/java/pe/com/bancom/BackendBancomApplication.java
@@ -0,0 +1,13 @@
+package pe.com.bancom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBancomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBancomApplication.class, args);
+	}
+
+}
---
+++ backend-bancom/src/test/java/pe/com/bancom/BackendBancomApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.bancom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBancomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
