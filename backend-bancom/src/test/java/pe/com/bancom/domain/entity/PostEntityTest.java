86a7bb71fe0e385aadd0c58c1c1a3b93b71a64d6
Ronald Angulo <ing.ronaldangulo@gmail.com>
2023-11-17 08:42:41 -0500

    Test entity post falla

---
+++ backend-bancom/src/test/java/pe/com/bancom/domain/entity/PostEntityTest.java
@@ -0,0 +1,22 @@
+package pe.com.bancom.domain.entity;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PostEntityTest {
+
+    @Test
+    void testEntityExist(){
+        PostEntity postEntity = PostEntity.builder()
+                .id(1)
+                .text("Mi primer post")
+                .dateCreation(new Date())
+                .build();
+
+        assertEquals(1, postEntity.getId());
+        assertEquals("Ronald",postEntity.getText());
+    }
+}
